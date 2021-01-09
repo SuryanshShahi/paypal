d9b602c2f79e298aeacc3eafc7879efb0620b357
SuryanshShahi <suryansh06shahi@gmail.com>
2021-01-10 02:36:12 +0530

    add all files

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const paypal = require('paypal-rest-sdk');
+
+paypal.configure({
+  'mode': 'sandbox', //sandbox or live
+  'client_id': 'ATXuWWtloISyJ6UZGCvn6xBTeyFVpEu2sZ7DuFH_2QD8s9MCOnO5bmZKHg0ySHx4o5_ghSbCpZetwtVP',
+  'client_secret': 'EJyd8aa1q2qcOVIhjsltMv6uCmMfkNnBDlUFhH0iHAtEdZuqL5snxB37kq0T9c6J4ozBaLgvRczV0ptI'
+});
+
+const app = express();
+
+app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"));
+
+app.post('/pay', (req, res) => {
+    const create_payment_json = {
+      "intent": "sale",
+      "payer": {
+          "payment_method": "paypal"
+      },
+      "redirect_urls": {
+          "return_url": "http://localhost:3000/success",
+          "cancel_url": "http://localhost:3000/cancel"
+      },
+      "transactions": [{
+          "item_list": {
+              "items": [{
+                  "name": "Red Sox Hat",
+                  "sku": "001",
+                  "price": "25.00",
+                  "currency": "USD",
+                  "quantity": 1
+              }]
+          },
+          "amount": {
+              "currency": "USD",
+              "total": "25.00"
+          },
+          "description": "Hat for the best team ever"
+      }]
+  };
+  
+  paypal.payment.create(create_payment_json, function (error, payment) {
+    if (error) {
+        throw error;
+    } else {
+        for(let i = 0;i < payment.links.length;i++){
+          if(payment.links[i].rel === 'approval_url'){
+            res.redirect(payment.links[i].href);
+          }
+        }
+    }
+  });
+  
+  });
+
+  app.get('/success', (req, res) => {
+    const payerId = req.query.PayerID;
+    const paymentId = req.query.paymentId;
+  
+    const execute_payment_json = {
+      "payer_id": payerId,
+      "transactions": [{
+          "amount": {
+              "currency": "USD",
+              "total": "25.00"
+          }
+      }]
+    };
+  
+    paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
+      if (error) {
+          console.log(error.response);
+          throw error;
+      } else {
+          console.log(JSON.stringify(payment));
+          res.send('Success');
+      }
+  });
+  });
+
+  app.get('/cancel', (req, res) => res.send('Cancelled'));
+  
+app.listen(4004, () => console.log("App is running at port 4004..."));
